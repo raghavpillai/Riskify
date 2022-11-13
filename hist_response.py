a107f50c11f7b824e46646fcd7f34d1fc0c56d6c
Raghav <raghavpillai101@gmail.com>
2022-11-12 18:15:13 -0600

    Analyzer

--- hist_response.py
+++ hist_response.py
@@ -1,29 +1,33 @@
 import json
 import requests
+import os
 
-#Opening JSON file
-f = open('historical.json')
-  
-data = json.load(f)
+goal_dir = os.path.join(os.getcwd(), "data/market/")
 
-values = []
-for i in data['data']:
-    values.append(i['close'])
-  
-#Closing file
-f.close()
 
-headers = {"Content-type": "application/json"}
-fields = {
-    "alpha": 0.05,
-    "portfolios": [
-        {
-            "portfolioValues": values
-        }
-    ]
-}
+tot_values = []
+for filename in os.listdir(goal_dir):
+    f = os.path.join(goal_dir, filename)
+    if os.path.isfile(f):
+        values = []
+        file = open(f)
+        data = json.load(file)
+        for i in data:
+            values.append(data[i]["Close"])
+        tot_values.append(values)
+        file.close()
+        
+for table in tot_values:
+    headers = {"Content-type": "application/json"}
+    fields = {
+        "alpha": 0.05,
+        "portfolios": [
+            {
+                "portfolioValues": table
+            }
+        ]
+    }
+    response = requests.post('https://api.portfoliooptimizer.io/v1/portfolio/analysis/conditional-value-at-risk', json=fields)
 
-response = requests.post('https://api.portfoliooptimizer.io/v1/portfolio/analysis/conditional-value-at-risk', json=fields)
-
-print("Status Code", response.status_code)
-print("JSON Response ", response.json())
+    print("Status Code", response.status_code)
+    print("JSON Response ", response.json())
